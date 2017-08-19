f6d955fa2aaf5fe59244868aabffbade9bc21f4c
Kelley Rose <krose422@gmail.com>
2017-08-19 11:04:11 -0400

    Create generageMessage utility

--- README.md
+++ README.md
@@ -5,3 +5,17 @@ This app is already on heroku at https://morning-lowlands-24374.herokuapp.com/
 
 ### Deploy to Heroku
 - `git push heroku master`
+
+### Development
+
+#### Setup
+`$ npm install`
+
+#### Run app on localhost:3000
+`$ nodemon server/server.js`
+
+### Tests
+These commands will run tests on any files ending in `test.js` using Mocha.
+
+`$ npm test`
+`$ nodemon run test-watch`
--- server/server.js
+++ server/server.js
@@ -3,6 +3,7 @@ const http = require('http');
 const express = require('express');
 const socketIO = require('socket.io');
 
+const {generateMessage} = require('./utils/message');
 const publicPath = path.join(__dirname, '../public');
 const port = process.env.PORT || 3000;
 var app = express();
@@ -17,23 +18,13 @@ io.on('connection', (socket) => {
   // socket.emit from admin, text welcome to the chat app
   // socket.broadcast.emit from admin, text new user joined
 
-  socket.emit('newMessage', {
-    from: 'Admin',
-    text: 'Welcome to the chat app'
-  });
+  socket.emit('newMessage', generateMessage('Admin', 'Welcome to the chat app'));
 
-  socket.broadcast.emit('newMessage', {
-    from: 'Admin',
-    text: 'New user joined'
-  });
+  socket.broadcast.emit('newMessage', generateMessage('Admin', 'New user joined'));
 
-  socket.on('createMessage', (newMessage) => {
-    console.log('New message arrived', newMessage);
-    io.emit('newMessage', {
-      from: newMessage.from,
-      text: newMessage.text,
-      createdAt: new Date().getTime()
-    });
+  socket.on('createMessage', (message) => {
+    console.log('New message arrived', message);
+    io.emit('newMessage', generateMessage(message.from, message.text));
 
     // socket.broadcast.emit('newMessage', {
     //   from: newMessage.from,
